82efeb81defe8122b5bdfa27ae2d88a09e990d12
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-09-01 18:08:52 -0500

    test docker 01-09-23T18:08

---
+++ target/classes/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO user(user, password, rol) VALUES
+    ("rrhh", "$2a$10$gKlrb5pX4zyXbO.may9CZeYqyMHrqgRdVjk.0A8hI8nfJenMNd9By", "ROLE_RRHH"), /*password: rrhh*/
+    ("healthPersonnel", "$2a$10$y69N3nQpTXwydOWpNo/eiuEMaVfCwQH.ET8DTxYBaHkrBVoQxb5Cq", "ROLE_HEALTH_PERSONNEL"),/*password: healthPersonnel*/
+    ("admin", "$2a$10$0XJN/wfeBHyJnAkURNMJNeyu5E62SIYEQH6ABd5ubvUGBFZxw1EDS", "ROLE_ADMIN"),/*password: admin*/
+    ("patient", "$2a$10$uw5WwlWRT4TVZNefUuaH5.D89zwJ5URF5Yuv.w2hj89xiEq72cfUW", "ROLE_PATIENT");/*password: patient*/
+/*
+INSERT INTO service_health(name) VALUES
+    ("service1"),
+    ("service2");
+
+INSERT INTO shift(name, start, end) VALUES
+    ("mañana", "10:00:00.234Z", "13:00:00.234Z"),
+    ("tarde", "13:00:00.234Z", "18:00:00.234Z");
+
+INSERT INTO patient() VALUES ();
+
+INSERT INTO consulting_room() VALUES ();
+
+INSERT INTO health_personnel() VALUES();
+
+INSERT INTO appointment() VALUES();
+
+INSERT INTO schedule() VALUES();
+*/
